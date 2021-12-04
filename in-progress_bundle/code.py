b324aefcca799aa54d052e9b85bc94a5fe4d7843
Cedar Grove Maker Studios <jan@goolsbey.net>
2021-12-03 23:32:18 -0800

    in-progress: add simulated NAU7802 driver; add button hold capability

--- in-progress_bundle/code.py
+++ in-progress_bundle/code.py
@@ -1,6 +1,6 @@
 # PyPortal Scale -- dual channel version
 # Cedar Grove NAU7802 FeatherWing
-# 2021-12-02 v2.2 Cedar Grove Studios
+# 2021-12-03 v2.3 Cedar Grove Studios
 
 # uncomment the following import line to run the calibration method
 # (this will eventually be put into the setup process)
@@ -11,6 +11,7 @@ import displayio
 import gc
 import time
 from cedargrove_nau7802 import NAU7802
+#from cedargrove_nau7802_fake import NAU7802
 import cedargrove_scale.display_graphics
 import cedargrove_scale.buttons_pyportal
 from cedargrove_scale.configuration import play_tone, dial_to_rect
@@ -25,7 +26,7 @@ DEBUG = False
 # Instantiate display groups and graphics
 dial = Scale(max_scale=100, center=(0.5, 0.55), size=0.52)
 labels = cedargrove_scale.display_graphics.Labels()
-panel = cedargrove_scale.buttons_pyportal.ScaleButtons(timeout=0.5, debug=DEBUG)
+panel = cedargrove_scale.buttons_pyportal.ScaleButtons(timeout=1.0, debug=DEBUG)
 
 # Instantiate SD card
 sd = SDCard()
@@ -39,20 +40,20 @@ def zero_channel():
     a raw zero offset value. Use when scale is started, a new channel is
     selected, or to adjust for measurement drift. NOTE: Remove weight and tare
     from load cell before executing."""
-    labels.status_label.text = " "
-    labels.status_label.text = "ZERO LOAD CELL " + str(nau7802.channel)
+    labels.status_label.text = ' '
+    labels.status_label.text = 'ZERO LOAD CELL ' + str(nau7802.channel)
     labels.status_label.color = Colors.YELLOW
     print(
-        "channel %1d calibrate.INTERNAL: %5s"
-        % (nau7802.channel, nau7802.calibrate("INTERNAL"))
+        'channel %1d calibrate.INTERNAL: %5s'
+        % (nau7802.channel, nau7802.calibrate('INTERNAL'))
     )
     print(
-        "channel %1d calibrate.OFFSET:   %5s"
-        % (nau7802.channel, nau7802.calibrate("OFFSET"))
+        'channel %1d calibrate.OFFSET:   %5s'
+        % (nau7802.channel, nau7802.calibrate('OFFSET'))
     )
     zero_offset = read(100)  # Average 100 samples to establish zero offset value
-    print("...channel zeroed")
-    labels.status_label.text = " "
+    print('...channel zeroed')
+    labels.status_label.text = ' '
     return zero_offset
 
 
@@ -110,7 +111,7 @@ display.brightness = Defaults.BRIGHTNESS
 scale_group = displayio.Group()
 
 # Define display background and displayio group elements
-print("*** Define display background and displayio group elements")
+print('*** Define display background and displayio group elements')
 
 # Bitmap background -- FUTURE FEATURE?
 """_bkg = displayio.OnDiskBitmap('/sd/background.bmp')
@@ -127,13 +128,13 @@ dial.value = (0, 0)
 display.show(scale_group)
 
 if sd.has_card:
-    labels.flash_status("SD CARD FOUND", 0.5)
+    labels.flash_status('SD CARD FOUND', 0.5)
 else:
-    labels.flash_status("NO SD CARD", 0.5)
+    labels.flash_status('NO SD CARD', 0.5)
 
 # Instantiate and calibrate load cell inputs
-print("*** Instantiate and calibrate load cells")
-print(" enable NAU7802 digital and analog power: %5s" % (nau7802.enable(True)))
+print('*** Instantiate and calibrate load cells')
+print(' enable NAU7802 digital and analog power: %5s' % (nau7802.enable(True)))
 
 nau7802.gain = Config.PGA_GAIN  # Use default gain
 nau7802.channel = 1  # Set to second channel
@@ -162,17 +163,17 @@ plot_tares()
 plot_alarms()
 
 if sd.has_card:
-    labels.flash_status("SCREENSHOT...", 0.8)
+    labels.flash_status('SCREENSHOT...', 0.8)
     labels.status_label.text = Defaults.NAME
     labels.status_label.color = Colors.CYAN
     sd.screenshot()
-    labels.flash_status("... STORED", 0.8)
+    labels.flash_status('... STORED', 0.8)
 else:
-    labels.flash_status("SCREENSHOT: NO SD CARD", 1.0)
+    labels.flash_status('SCREENSHOT: NO SD CARD', 1.0)
 
-labels.flash_status("READY", 0.5)
-play_tone("high")
-play_tone("low")
+labels.flash_status('READY', 0.5)
+play_tone('high')
+play_tone('low')
 
 # -- Main loop: Read sample, move bubble, and display values
 while True:
@@ -190,9 +191,9 @@ while True:
         tare = 0
     chan_1_mass_gr = round((value - chan_1_zero) * Config.CALIB_RATIO_1, 1) - tare
     chan_1_mass_oz = round(chan_1_mass_gr * 0.03527, 2)
-    if str(chan_1_mass_gr) == "-0.0":  # Filter -0.0 value
+    if str(chan_1_mass_gr) == '-0.0':  # Filter -0.0 value
         chan_1_mass_gr = 0.0
-    labels.chan_1_value.text = "%5.1f" % (chan_1_mass_gr)
+    labels.chan_1_value.text = '%5.1f' % (chan_1_mass_gr)
 
     nau7802.channel = 2
     value = read()
@@ -202,47 +203,47 @@ while True:
         tare = 0
     chan_2_mass_gr = round((value - chan_2_zero) * Config.CALIB_RATIO_2, 1) - tare
     chan_2_mass_oz = round(chan_2_mass_gr * 0.03527, 2)
-    if str(chan_2_mass_gr) == "-0.0":  # Filter -0.0 value
+    if str(chan_2_mass_gr) == '-0.0':  # Filter -0.0 value
         chan_2_mass_gr = 0.0
-    labels.chan_2_value.text = "%5.1f" % (chan_2_mass_gr)
+    labels.chan_2_value.text = '%5.1f' % (chan_2_mass_gr)
 
     chan_1_mass_gr_norm = chan_1_mass_gr / Defaults.MAX_GR
     chan_2_mass_gr_norm = chan_2_mass_gr / Defaults.MAX_GR
     dial.value = (chan_1_mass_gr_norm, chan_2_mass_gr_norm)
 
-    print("(%+5.1f, %+5.1f)" % (chan_1_mass_gr, chan_2_mass_gr))
+    print('(%+5.1f, %+5.1f)' % (chan_1_mass_gr, chan_2_mass_gr))
 
     labels.heartbeat(0)  # Maroon heartbeat indicator
 
     a1 = a2 = False
     if alarm_1_enable and chan_1_mass_gr >= alarm_1_mass_gr:
         a1 = True
-        play_tone("low")
+        play_tone('low')
         labels.status_label.color = Colors.RED
 
     if alarm_2_enable and chan_2_mass_gr >= Defaults.ALARM_2_MASS_GR:
         a2 = True
-        play_tone("high")
+        play_tone('high')
         labels.status_label.color = Colors.RED
 
     if a1 and a2:
         labels.status_label.text = (
-            "ALARM: " + Defaults.CHAN_1_NAME + " and " + Defaults.CHAN_2_NAME
+            'ALARM: ' + Defaults.CHAN_1_NAME + ' and ' + Defaults.CHAN_2_NAME
         ).upper()
         # labels.status_label.color = Colors.RED
     elif a1:
-        labels.status_label.text = ("ALARM: " + Defaults.CHAN_1_NAME).upper()
+        labels.status_label.text = ('ALARM: ' + Defaults.CHAN_1_NAME).upper()
         # labels.status_label.color = Colors.RED
     elif a2:
-        labels.status_label.text = ("ALARM: " + Defaults.CHAN_2_NAME).upper()
+        labels.status_label.text = ('ALARM: ' + Defaults.CHAN_2_NAME).upper()
         # labels.status_label.color = Colors.RED
     alarm = a1 or a2
 
     button_pressed, hold_time = panel.read_buttons()
-    if button_pressed in ("zero_1", "zero_2"):
+    if button_pressed in ('zero_1', 'zero_2'):
         # Zero and recalibrate channel
         channel = int(button_pressed[5])
-        play_tone("high")
+        play_tone('high')
         nau7802.channel = channel
 
         if channel == 1:
@@ -250,64 +251,73 @@ while True:
         else:
             chan_2_zero = zero_channel()
 
-    if button_pressed in ("tare_1", "tare_2"):
-        # Enable/disable tares
+    if button_pressed in ('tare_1', 'tare_2'):
         channel = int(button_pressed[5])
-        play_tone("high")
-        nau7802.channel = channel
+        play_tone('high')
+        if hold_time <= panel.timeout:
+            # Enable/disable tares
+            nau7802.channel = channel
 
-        if channel == 1:
-            tare_1_enable = not tare_1_enable  # toggle tare 1 state
-            if tare_1_enable:
-                # labels.flash_status('TARE 1 ENABLE', 0.5)
-                if str(tare_1_mass_gr) == "-0.0":  # Filter -0.0 value
-                    tare_1_mass_gr = 0.0
-                labels.tare_1_value.color = Colors.ORANGE
-                panel.tare_1_icon[0] = 1
+            if channel == 1:
+                tare_1_enable = not tare_1_enable  # toggle tare 1 state
+                if tare_1_enable:
+                    # labels.flash_status('TARE 1 ENABLE', 0.5)
+                    if str(tare_1_mass_gr) == '-0.0':  # Filter -0.0 value
+                        tare_1_mass_gr = 0.0
+                    labels.tare_1_value.color = Colors.ORANGE
+                    panel.tare_1_icon[0] = 1
+                else:
+                    # labels.flash_status('TARE 1 DISABLE', 0.5)
+                    labels.tare_1_value.color = Colors.GRAY
+                    panel.tare_2_icon[0] = 3
             else:
-                # labels.flash_status('TARE 1 DISABLE', 0.5)
-                labels.tare_1_value.color = Colors.GRAY
-                panel.tare_2_icon[0] = 3
+                tare_2_enable = not tare_2_enable  # toggle tare 2 state
+                if tare_2_enable:
+                    # labels.flash_status('TARE 2 ENABLE', 0.5)
+                    if str(tare_2_mass_gr) == '-0.0':  # Filter -0.0 value
+                        tare_2_mass_gr = 0.0
+                    labels.tare_1_value.color = Colors.ORANGE
+                    panel.tare_1_icon[0] = 5
+                else:
+                    # labels.flash_status('TARE 2 DISABLE', 0.5)
+                    labels.tare_2_value.color = Colors.GRAY
+                    panel.tare_2_icon[0] = 7
+            plot_tares()
         else:
-            tare_2_enable = not tare_2_enable  # toggle tare 2 state
-            if tare_2_enable:
-                # labels.flash_status('TARE 2 ENABLE', 0.5)
-                if str(tare_2_mass_gr) == "-0.0":  # Filter -0.0 value
-                    tare_2_mass_gr = 0.0
-                labels.tare_1_value.color = Colors.ORANGE
-                panel.tare_1_icon[0] = 5
-            else:
-                # labels.flash_status('TARE 2 DISABLE', 0.5)
-                labels.tare_2_value.color = Colors.GRAY
-                panel.tare_2_icon[0] = 7
-        plot_tares()
+            play_tone('high')
+            play_tone('high')
+            print('*** set tare', button_pressed)
 
-    if button_pressed in ("alarm_1", "alarm_2"):
+    if button_pressed in ('alarm_1', 'alarm_2'):
         # Enable/disable alarms
         channel = int(button_pressed[6])
-        play_tone("high")
-
-        if channel == 1:
-            alarm_1_enable = not alarm_1_enable  # toggle alarm 1 state
-            if alarm_1_enable:
-                # labels.flash_status('ALARM 1 ENABLE', 0.5)
-                labels.alarm_1_value.color = Colors.ORANGE
-                panel.alarm_1_icon[0] = 0
+        play_tone('high')
+        if hold_time <= panel.timeout:
+            if channel == 1:
+                alarm_1_enable = not alarm_1_enable  # toggle alarm 1 state
+                if alarm_1_enable:
+                    # labels.flash_status('ALARM 1 ENABLE', 0.5)
+                    labels.alarm_1_value.color = Colors.ORANGE
+                    panel.alarm_1_icon[0] = 0
+                else:
+                    # labels.flash_status('ALARM 1 DISABLE', 0.5)
+                    labels.alarm_1_value.color = Colors.GRAY
+                    panel.alarm_1_icon[0] = 2
             else:
-                # labels.flash_status('ALARM 1 DISABLE', 0.5)
-                labels.alarm_1_value.color = Colors.GRAY
-                panel.alarm_1_icon[0] = 2
+                alarm_2_enable = not alarm_2_enable  # toggle alarm 1 state
+                if alarm_2_enable:
+                    # labels.flash_status('ALARM 2 ENABLE', 0.5)
+                    labels.alarm_2_value.color = Colors.GREEN
+                    panel.alarm_2_icon[0] = 4
+                else:
+                    # labels.flash_status('ALARM 2 DISABLE', 0.5)
+                    labels.alarm_2_value.color = Colors.GRAY
+                    panel.alarm_2_icon[0] = 6
+            plot_alarms()
         else:
-            alarm_2_enable = not alarm_2_enable  # toggle alarm 1 state
-            if alarm_2_enable:
-                # labels.flash_status('ALARM 2 ENABLE', 0.5)
-                labels.alarm_2_value.color = Colors.GREEN
-                panel.alarm_2_icon[0] = 4
-            else:
-                # labels.flash_status('ALARM 2 DISABLE', 0.5)
-                labels.alarm_2_value.color = Colors.GRAY
-                panel.alarm_2_icon[0] = 6
-        plot_alarms()
+            play_tone('high')
+            play_tone('high')
+            print('*** set alarm', channel)
 
     """if button.name in ('setup_1', 'setup_2'):
         # Initiate the setup process for a channel
@@ -324,4 +334,4 @@ while True:
     gc.collect()
     free_memory = gc.mem_free()
     frame = time.monotonic() - t0
-    print(f"frame: {frame:5.2f} sec   free memory: {free_memory} bytes")
+    print(f'frame: {frame:5.2f} sec   free memory: {free_memory} bytes')
