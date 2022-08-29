f99ca0a1b02dab4c21e5711134d22986e9f2c0df
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-08-29 15:44:15 -0700

    update code.py with failover scheme

--- code/code.py
+++ code/code.py
@@ -24,4 +24,28 @@ __repo__ = "https://github.com/CedarGroveStudios/Scale"
 # Uncomment the following to run the load cell calibration method
 # import cedargrove_scale.load_cell_calibrator
 
-import scale_code
+
+while True:
+    """Attempt to start the primary code module. Upon failure, dim the display
+    to keep the board cooler and flash the NeoPixel for attention."""
+
+    try:
+        import scale_code
+    except Exception as e:
+        import board
+        import neopixel
+        import time
+
+        status = neopixel.NeoPixel(board.NEOPIXEL, 1)
+        status[0] = 0x040000  # Glow red on exception
+        board.DISPLAY.brightness = 0.5  # Dim the REPL display to keep things cool
+
+        print(f"scale_code.py: {e}  time.monotonic: {time.monotonic()}")
+
+        while True:
+            """Flash the NeoPixel red forever."""
+            time.sleep(1)
+            if status[0] != (0, 0, 0):
+                status[0] = 0x000000  # Flash red on exception
+            else:
+                status[0] = 0x040000  # Flash red on exception
